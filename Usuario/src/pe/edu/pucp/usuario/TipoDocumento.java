a0e93e530fced8657e507907f1a758406a5b48cf
Jesus Huayhua <106457985+JesusHuayhua@users.noreply.github.com>
2024-09-10 12:29:50 -0500

    CreacionClases
    Creacion de los proyectos de tipo class:
    - Inventario
    - Trabajadores
    - Usuario
    - Ventas

---
+++ Usuario/src/pe/edu/pucp/usuario/TipoDocumento.java
@@ -0,0 +1,7 @@
+package pe.edu.pucp.usuario;
+
+public enum TipoDocumento {
+    DNI,
+    CARNET_DE_EXTRANJERIA,
+    PASAPORTE
+}
---
+++ Ventas/src/pe/edu/pucp/metodoDePago/Factura.java
@@ -0,0 +1,44 @@
+package pe.edu.pucp.metodoDePago;
+
+import java.util.Date;
+
+
+public class Factura extends MetodoDePago {
+    private static Integer numeroDeFactura = 0;
+    private String ruc;
+    private String razonSocial;
+    private String direccionDeFacturacion;
+
+    public Factura(String ruc, String razonSocial, String direccionDeFacturacion, Date fecha, Double subtotal, Double total, Double igv, Double descuentoAplicado) {
+        super(fecha, subtotal, total, igv, descuentoAplicado);
+        this.ruc = ruc;
+        this.razonSocial = razonSocial;
+        this.direccionDeFacturacion = direccionDeFacturacion;
+        this.numeroDeFactura++;
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getDireccionDeFacturacion() {
+        return direccionDeFacturacion;
+    }
+
+    public void setDireccionDeFacturacion(String direccionDeFacturacion) {
+        this.direccionDeFacturacion = direccionDeFacturacion;
+    }
+    
+}
