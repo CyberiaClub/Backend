90f4c925b3cdb1ef2b7652c8897205290b3d2d90
Jesus Huayhua <106457985+JesusHuayhua@users.noreply.github.com>
2024-11-04 00:53:47 -0500

    change database
    Cambios en la base de datos, para soportar imagenes

--- CyberiaStoreDBManagerTest/src/cyberiastoredbmanagertest/CyberiaStoreDBManagerTest.java
+++ CyberiaStoreDBManagerTest/src/cyberiastoredbmanagertest/CyberiaStoreDBManagerTest.java
@@ -1,17 +1,7 @@
 package cyberiastoredbmanagertest;
 
-import java.lang.System.Logger;
-import java.lang.System.Logger.Level;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import pe.edu.pucp.cyberiastore.util.Cifrado;
-
 public class CyberiaStoreDBManagerTest {
 
     public static void main(String[] args) {
-        String contrasenha = "EKQ1Q2/R2335";
-        Cifrado cifrado = new Cifrado();
-        String c = cifrado.cifrarMD5(contrasenha);
-        System.out.println(c);
     }
 }
